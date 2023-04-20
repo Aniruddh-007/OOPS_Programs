2e56afbeb2f408cfc174a791e18795c13902a83e
Aniruddh-007 <127217429+Aniruddh-007@users.noreply.github.com>
2023-04-20 09:43:18 +0530

    Create courses.c

---
+++ Practice/courses.c
@@ -0,0 +1,26 @@
+//Question 01 CODECHEF
+/*
+In the new CodeChef Learn module, under the "Learn Problem Solving" section, there are two courses for each language. For eg. "Python Beginner - Part 1" and "Python Beginner - Part 2". These courses help you get started with CodeChef contests.
+
+Currently there are courses for 4 languages, and hence there are 8 courses in this section. But suppose there are courses for 
+�
+N languages, what will be the total number of courses in this section?
+
+Input Format
+The only line of input will contain a single integer 
+�
+N, denoting the number of languages for which there are courses.
+
+Output Format
+Output on a single line the total number of courses in the section.
+*/
+
+#include <stdio.h>
+
+int main(void) 
+{
+	int n ;
+	scanf("%d",&n);
+	printf("%d",2*n);
+	return 0;
+}
