1f46579560cff24688e7ab323255542b802c615b
Aniruddh-007 <127217429+Aniruddh-007@users.noreply.github.com>
2023-06-17 10:16:11 +0530

    Create Celcius to Fahrenheit.cpp

---
+++ C++ Programs/Celcius to Fahrenheit.cpp
@@ -0,0 +1,18 @@
+//Fahrenheit to Celcius
+#include <iostream>
+using namespace std;
+
+// Function to convert Celcius to Fahrenheit
+float Conversion(float n)
+{
+	return [n * 9.0) / 5.0] + 32;
+}
+
+int main()
+{
+	float n ;
+  cout<<"Enter Celcius : ";
+  cin>>n;
+	cout << Conversion(n);
+	return 0;
+}
